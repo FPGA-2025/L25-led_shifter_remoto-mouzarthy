8da4a3382c0105cb463248720731e3e79d8d37ec
mouzarthy <mouzarthy2007@hotmail.com>
2025-07-11 17:56:34 -0300

    l25 shifter

--- led_shifter.v
+++ led_shifter.v
@@ -6,5 +6,29 @@ module LedShifter #(
     output  reg [7:0] leds
 );
 
+localparam CICLO = CLK_FREQ / 4;
+reg [31:0] cont;
+
+always @( posedge clk or negedge rst_n ) 
+begin
+    if( !rst_n )
+    begin
+        leds = 8'b00011111;
+        cont = 0;
+    end
+    else
+    begin
+        if( cont >= CICLO - 1 ) 
+        begin
+          cont <= 0;
+          leds <= { leds[6:0], leds[7] };  
+        end
+        else
+        begin
+            cont <= cont + 1;
+        end
+    end
+    
+end
     
 endmodule
